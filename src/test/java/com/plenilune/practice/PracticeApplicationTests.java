09e9c9726cabf8a2349e8985d861b34a0bb5999f
Mingyu Kang <plenilune095@gmail.com>
2020-08-12 00:02:02 +0900

    init

---
+++ src/test/java/com/plenilune/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plenilune.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
