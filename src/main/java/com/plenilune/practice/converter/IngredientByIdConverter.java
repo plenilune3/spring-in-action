716a06f0c0096ae88d95a83bcd89856dfa7dbe50
Mingyu Kang <plenilune095@gmail.com>
2020-08-12 15:24:39 +0900

    재료를 객체로 변환하기 위한 컨버터 작성

---
+++ src/main/java/com/plenilune/practice/converter/IngredientByIdConverter.java
@@ -0,0 +1,23 @@
+package com.plenilune.practice.converter;
+
+import com.plenilune.practice.dao.IngredientRepository;
+import com.plenilune.practice.domain.Ingredient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+
+    private IngredientRepository ingredientRepository;
+
+    @Autowired
+    public IngredientByIdConverter(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @Override
+    public Ingredient convert(String id) {
+        return ingredientRepository.findById(id);
+    }
+}
