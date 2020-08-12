083570ef318b55c9c574937703c6d632a71422a5
Mingyu Kang <plenilune095@gmail.com>
2020-08-12 09:52:54 +0900

    DesignTacoController에 IngredientRepository를 주입하고 사용하기
     - 데이터베이스를 대신하던 기존 코드 삭

--- src/main/java/com/plenilune/practice/controller/DesignTacoController.java
+++ src/main/java/com/plenilune/practice/controller/DesignTacoController.java
@@ -1,10 +1,12 @@
 package com.plenilune.practice.controller;
 
+import com.plenilune.practice.dao.IngredientRepository;
 import com.plenilune.practice.domain.Ingredient;
 import com.plenilune.practice.domain.Taco;
 import com.plenilune.practice.domain.Ingredient.Type;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -13,7 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,18 +24,13 @@ import java.util.stream.Collectors;
 @RequestMapping("/design")
 public class DesignTacoController {
 
-    List<Ingredient> ingredients = Arrays.asList(
-            new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-            new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-            new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-            new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-            new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-            new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-            new Ingredient("CHED", "Cheddar", Type.CHEESE),
-            new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-            new Ingredient("SLSA", "Salsa", Type.SAUCE),
-            new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-    );
+    private final IngredientRepository ingredientRepository;
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
 
     @GetMapping
     public String showDesignForm(Model model) {
@@ -59,6 +56,9 @@ public class DesignTacoController {
     }
 
     private void sortByType(Model model) {
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredientRepository.findAll().forEach(ingredient -> ingredients.add(ingredient));
+
         Type[] types = Type.values();
 
         for (Type type : types) {
