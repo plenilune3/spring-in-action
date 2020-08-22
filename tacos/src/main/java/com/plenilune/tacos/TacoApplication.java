d050bf17861f99f99e26dfff99a09c6eadbcb6a5
Mingyu Kang <plenilune095@gmail.com>
2020-08-22 21:52:57 +0900

    코드 수정

--- tacos/src/main/java/com/plenilune/tacos/TacoApplication.java
+++ tacos/src/main/java/com/plenilune/tacos/TacoApplication.java
@@ -1,13 +1,18 @@
 package com.plenilune.tacos;
 
+import org.apache.catalina.security.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
-@EntityScan("com.plenilune.tacodomain")
+@SpringBootApplication(scanBasePackages = "com.plenilune.*")
+@EntityScan(basePackages = "com.plenilune.*")
 public class TacoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TacoApplication.class, args);
     }
