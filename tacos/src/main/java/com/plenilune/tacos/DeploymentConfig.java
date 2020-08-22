ba55089251476d5ad0a1a384ed22978839d708af
Mingyu Kang <plenilune095@gmail.com>
2020-08-22 21:51:50 +0900

    데이터베이스 초기 설정
     - 테스트를 위한 Taco 데이터 삽입 코드 작

--- tacos/src/main/java/com/plenilune/tacos/DeploymentConfig.java
+++ tacos/src/main/java/com/plenilune/tacos/DeploymentConfig.java
@@ -1,16 +1,51 @@
 package com.plenilune.tacos;
 
-import com.plenilune.tacodata.IngredientRepository;
+import com.plenilune.tacodata.TacoService;
+import com.plenilune.tacodata.UserService;
 import com.plenilune.tacodomain.Ingredient;
 import com.plenilune.tacodomain.Ingredient.Type;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.plenilune.tacodomain.Taco;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("!prod")
+import java.util.Arrays;
+
 @Configuration
 public class DeploymentConfig {
 
+    @Bean
+    public CommandLineRunner dataLoader(TacoService tacoService) { // user repo for ease of testing with a built-in user
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                Ingredient flourTortilla = new Ingredient("FLTO", "Flour Tortilla", Type.WRAP);
+                Ingredient cornTortilla = new Ingredient("COTO", "Corn Tortilla", Type.WRAP);
+                Ingredient groundBeef = new Ingredient("GRBF", "Ground Beef", Type.PROTEIN);
+                Ingredient carnitas = new Ingredient("CARN", "Carnitas", Type.PROTEIN);
+                Ingredient tomatoes = new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES);
+                Ingredient lettuce = new Ingredient("LETC", "Lettuce", Type.VEGGIES);
+                Ingredient cheddar = new Ingredient("CHED", "Cheddar", Type.CHEESE);
+                Ingredient jack = new Ingredient("JACK", "Monterrey Jack", Type.CHEESE);
+                Ingredient salsa = new Ingredient("SLSA", "Salsa", Type.SAUCE);
+                Ingredient sourCream = new Ingredient("SRCR", "Sour Cream", Type.SAUCE);
+
+                Taco taco1 = new Taco();
+                taco1.setName("Carnivore");
+                taco1.setIngredients(Arrays.asList(flourTortilla, groundBeef, carnitas, sourCream, salsa, cheddar));
+                tacoService.save(taco1);
+
+                Taco taco2 = new Taco();
+                taco2.setName("Bovine Bounty");
+                taco2.setIngredients(Arrays.asList(cornTortilla, groundBeef, cheddar, jack, sourCream));
+                tacoService.save(taco2);
+
+                Taco taco3 = new Taco();
+                taco3.setName("Veg-Out");
+                taco3.setIngredients(Arrays.asList(flourTortilla, cornTortilla, tomatoes, lettuce, salsa));
+                tacoService.save(taco3);
+            }
+        };
+    }
 }
